cb979f68e82351b801b62b1b62a0de7d06c223f6
Florent-W <35995337+Florent-W@users.noreply.github.com>
2024-04-22 10:08:08 +0200

    Ajout de la demande de seed, des states des robots, d'une cible, d'un mode pour voir les cases découvertes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate noise;
 use bevy::{input::mouse::MouseWheel, prelude::*};
+use bevy::input::keyboard::KeyboardInput;
+use bevy::input::ButtonState;
 use noise::{NoiseFn, Perlin, Seedable};
 use rand::Rng;
 use std::f32::consts::PI;
+use std::io;
 
 const ENERGIE_SPRITE: &str = "textures/energie.png";
 const MINERAL_SPRITE: &str = "textures/minerai.png";
@@ -57,6 +60,21 @@ enum TypeRobot {
     Visiteur,
 }
 
+#[derive(Resource, Debug)]
+struct AffichageCasesNonDecouvertes(bool); 
+
+#[derive(Resource, Debug)]
+struct SeedResource {
+    seed: Option<u32>,
+}
+
+#[derive(Component, PartialEq, Debug)]
+enum RobotState {
+    Exploring,
+    Returning,
+    AtBase,
+}
+
 #[derive(Component, Debug)]
 struct Robot {
     id: i32,
@@ -64,7 +82,9 @@ struct Robot {
     pv_max: i32,
     type_robot: TypeRobot,
     vitesse: i32,
-    timer: f32
+    timer: f32,
+    target_position: Option<Position>,
+    steps_moved: i32, 
 }
 
 #[derive(Component, Debug)]
@@ -107,6 +127,7 @@ fn setup_camera(mut commands: Commands) {
 fn setup_map(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    seed_res: Res<SeedResource>
 ) {
     // Charger les textures pour les différents éléments de la carte
     let energie_texture_handle = asset_server.load(ENERGIE_SPRITE);
@@ -126,7 +147,12 @@ fn setup_map(
     // Créer l'entité de la carte avec sa position de base
     commands.spawn((Carte { largeur, hauteur }, Position { x: 0, y: 0 }));
 
-    let seed = rand::thread_rng().gen();
+    let seed = seed_res.seed.unwrap_or_else(|| {
+        let random_seed = rand::thread_rng().gen();
+        println!("Pas de seed donné");
+        random_seed
+    });
+    println!("Seed utilisé : {}", seed);
     let perlin = Perlin::new(seed);
 
     // Génération des éléments de la carte en fonction de la valeur du noise
@@ -154,6 +180,7 @@ fn setup_map(
                         texture: texture_handle,
                         transform: Transform::from_translation(Vec3::new(x as f32, y as f32, 0.0))
                                    .with_scale(Vec3::splat(taille)),
+                        visibility: Visibility::Visible,           
                         ..Default::default()
                     })
                     .insert(ElementCarte {
@@ -166,16 +193,31 @@ fn setup_map(
     }
 
     // Ajout de la base sur la carte
-    let base_x = rand::thread_rng().gen_range(0..largeur) as i32;
-    let base_y = rand::thread_rng().gen_range(0..hauteur) as i32;
-    commands.spawn(SpriteBundle {
-        texture: base_handle,
-        transform: Transform::from_translation(Vec3::new(base_x as f32, base_y as f32, 0.0))
-                   .with_scale(Vec3::splat(0.002)),
-        ..Default::default()
-    })
-    .insert(Base)
-    .insert(Position { x: base_x, y: base_y }); 
+    let mut base_x: i32;
+    let mut base_y: i32;
+loop {
+    base_x = rand::thread_rng().gen_range(0..largeur) as i32;
+    base_y = rand::thread_rng().gen_range(0..hauteur) as i32;
+    
+    // Calcule la valeur du bruit pour la position générée
+    let noise_value = perlin.get([base_x as f64 * 0.1, base_y as f64 * 0.1]);
+    let noise_normalised = (noise_value + 1.0) / 2.0;
+
+    // Vérifie si la position n'est pas un obstacle
+    if noise_normalised <= 0.8 {
+        break; 
+    }
+}
+
+// Place la base à la position valide trouvée
+commands.spawn(SpriteBundle {
+    texture: base_handle,
+    transform: Transform::from_translation(Vec3::new(base_x as f32, base_y as f32, 0.0))
+               .with_scale(Vec3::splat(0.002)),
+    ..Default::default()
+})
+.insert(Base)
+.insert(Position { x: base_x, y: base_y });
 }
 
 /***
@@ -252,6 +294,10 @@ fn spawn_robots(
                 TypeRobot::Visiteur => format!("Visiteur{}", id),
             };
 
+            // Assign a random target position on the map
+            let target_x: i32 = rand::thread_rng().gen_range(0..carte.largeur) as i32;
+            let target_y: i32 = rand::thread_rng().gen_range(0..carte.hauteur) as i32;
+
             let timer = 1.0 / vitesse as f32;
 
             commands.spawn(SpriteBundle {
@@ -269,8 +315,11 @@ fn spawn_robots(
                 pv_max: 100,
                 type_robot: type_robot,
                 vitesse: vitesse,
-                timer: timer
-            }).insert(Position { x: robot_x, y: robot_y });
+                timer: timer,
+                target_position: Some(Position { x: target_x, y: target_y }),
+                steps_moved: 0 
+            }).insert(Position { x: robot_x, y: robot_y })
+            .insert(RobotState::Exploring);
         }
     }
 }
@@ -284,24 +333,24 @@ fn collect_resources_system(
     mut element_carte_query: Query<(Entity, &mut ElementCarte, &Position)>,
 ) {
     for (robot_entity, mut robot, robot_position) in robot_query.iter_mut() {
-        println!("{:?}", robot.type_robot);
+       // println!("{:?}", robot.type_robot);
         if robot.type_robot == TypeRobot::Collecteur {
-        println!("Checking robot {} at position {:?}", robot.nom, robot_position); 
+       // println!("Checking robot {} at position {:?}", robot.nom, robot_position); 
         let mut resource_collected = false; 
         for (entity, mut element_carte, resource_position) in element_carte_query.iter_mut() {
             if robot_position == resource_position {
                 resource_collected = true; // La ressource a été trouvée à la même position que le robot
                 match element_carte.element {
                     ElementMap::Ressource(Ressource::Energie) => {
-                        println!("Robot {} collected energy at position {:?}", robot.nom, robot_position);
+                      //  println!("Robot {} collected energy at position {:?}", robot.nom, robot_position);
                         commands.entity(entity).despawn();
                     },
                     ElementMap::Ressource(Ressource::Mineral) => {
-                        println!("Robot {} collected mineral at position {:?}", robot.nom, robot_position); 
+                      //  println!("Robot {} collected mineral at position {:?}", robot.nom, robot_position); 
                         commands.entity(entity).despawn();
                     },
                     ElementMap::Ressource(Ressource::LieuInteretScientifique) => {
-                        println!("Robot {} discovered a place of interest at position {:?}", robot.nom, robot_position); 
+                      //  println!("Robot {} discovered a place of interest at position {:?}", robot.nom, robot_position); 
                     },
                     _ => {
                     }
@@ -312,8 +361,27 @@ fn collect_resources_system(
             }
         }
         if !resource_collected {
-            println!("Robot {} did not collect any resources at position {:?}", robot.nom, robot_position); 
+           // println!("Robot {} did not collect any resources at position {:?}", robot.nom, robot_position); 
         }
+        }
+    }
+}
+
+fn update_robot_state(
+    mut commands: Commands,
+    mut query: Query<(&mut Robot, &Position, &mut RobotState)>,
+    base_query: Query<(&Base, &Position)>,
+) {
+    let (_, base_pos) = base_query.single();
+
+    for (mut robot, robot_pos, mut state) in query.iter_mut() {
+        // If the robot has no target and has moved 30 steps, it should return
+        if robot.target_position.is_none() && *state == RobotState::Exploring {
+            *state = RobotState::Returning;
+        }
+        // Update state to AtBase if the robot reaches the base
+        if *state == RobotState::Returning && robot_pos.x == base_pos.x && robot_pos.y == base_pos.y {
+            *state = RobotState::AtBase;
         }
     }
 }
@@ -344,6 +412,48 @@ fn move_camera_system(
     }
 }
 
+/***  
+ * Permet de demander un seed à l'utilisateur
+ * */
+fn request_seed_from_user() -> Option<u32> {
+    println!("Veuillez entrer un seed (nombre) ou appuyez sur entrer pour prendre un seed aléatoire:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Erreur dans la lecture de la ligne");
+
+    if input.trim().is_empty() {
+        None
+    } else {
+        input.trim().parse::<u32>().ok()
+    }
+}
+
+fn toggle_cases_non_decouvertes(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut affichage: ResMut<AffichageCasesNonDecouvertes>,
+) {
+
+    // Bascule l'état d'affichage quand la touche Tab est pressée           
+    if keyboard_input.just_pressed(KeyCode::Tab) {
+        affichage.0 = !affichage.0;
+        println!("Affichage cases non découvertes: {:?}", affichage.0);
+              print!("test");
+    }
+}
+
+fn adjust_visibility_system(
+    affichage_cases: Res<AffichageCasesNonDecouvertes>,
+    mut query: Query<(&ElementCarte, &mut Visibility)>,
+) {
+    for (element_carte, mut visibility) in query.iter_mut() {
+        if !affichage_cases.0 && (element_carte.est_decouvert == EtatDecouverte::NonDecouvert || element_carte.est_decouvert == EtatDecouverte::EnAttente) {
+            *visibility = Visibility::Hidden; 
+        } else {
+            *visibility = Visibility::Visible;
+        }
+    }
+}
+
+
 /***
  * Fonction pour faire un zoom avec la caméra
  */
@@ -368,33 +478,112 @@ fn zoom_camera_system(
  * Fonction pour déplacer les robots
  */
 fn move_robots_on_map_system(
-    mut query: Query<(Entity, &mut Position, &mut Transform, &mut Robot)>,
+    mut query: Query<(Entity, &mut Position, &mut Transform, &mut Robot, &mut RobotState)>,
     carte_query: Query<&Carte>,
+    base_query: Query<(&Base, &Position), Without<Robot>>,
     time: Res<Time>,
 ) {
-    let delta = time.delta_seconds();
-    let carte = carte_query.single();
+    let delta = time.delta_seconds(); 
+    let carte = carte_query.single();  
+    let (_, base_pos) = base_query.single();  // Position de la base
 
-    for (entity, mut position, mut transform, mut robot) in query.iter_mut() {
-        robot.timer -= delta;
+    for (entity, mut position, mut transform, mut robot, mut state) in query.iter_mut() {
+        robot.timer -= delta;  // 
 
-        // Vérifie si le timer du robot est terminé
-        if robot.timer <= 0.0 {
-            // Le timer est terminé, mouvement du robot
-            position.x = (position.x + 1) % carte.largeur as i32;
-            position.y = (position.y + 1) % carte.hauteur as i32;
+        match *state {
+            RobotState::Exploring => {  
+                if robot.timer <= 0.0 {  
+                    if let Some(target_position) = &robot.target_position { 
+                        if *position != *target_position && robot.steps_moved < 30 {  // Si la cible n'a pas été atteinte
+                            // Calculer la prochaine position
+                            position.x = (position.x + (target_position.x - position.x).signum()) % carte.largeur as i32;
+                            position.y = (position.y + (target_position.y - position.y).signum()) % carte.hauteur as i32;
+                            robot.steps_moved += 1;  
+                        } else {
+                            // Cible atteinte
+                            robot.target_position = None;
+                            robot.steps_moved = 0;
+                            *state = RobotState::Returning;
+                        }
+                    }
+                    robot.timer = 1.0 / robot.vitesse as f32; 
+                }
+            },
+            RobotState::Returning => {
+                if robot.timer <= 0.0 {
+                    if *position != *base_pos {
+                        position.x = (position.x + (base_pos.x - position.x).signum()) % carte.largeur as i32;
+                        position.y = (position.y + (base_pos.y - position.y).signum()) % carte.hauteur as i32;
+                    } else {
+                        *state = RobotState::AtBase;
+                        robot.timer = 5.0; // Attente de 5 secondes à la base
+                    }
+                    robot.timer = 1.0 / robot.vitesse as f32; 
+                } else {
+                    robot.timer -= delta;
+                }
+            }            
             
-            transform.translation.x = position.x as f32;
-            transform.translation.y = position.y as f32;
-
-            // Réinitialise le timer du robot basé sur sa vitesse
-            robot.timer = 1.0 / robot.vitesse as f32;
+            RobotState::AtBase => {
+                if robot.timer <= 0.0 { 
+                    // On cherche une position random sur la map en cible
+                    let target_x = rand::thread_rng().gen_range(0..carte.largeur) as i32;
+                    let target_y = rand::thread_rng().gen_range(0..carte.hauteur) as i32;
+                    robot.target_position = Some(Position { x: target_x, y: target_y });
+                    robot.steps_moved = 0;
+            
+                    *state = RobotState::Exploring; 
+                    robot.timer = 1.0 / robot.vitesse as f32; 
+                } else {
+                    robot.timer -= delta;
+                }
+            }            
+            
         }
+
+        transform.translation.x = position.x as f32;
+        transform.translation.y = position.y as f32;
     }
 }
 
 
+fn discover_elements(
+    mut commands: Commands,
+    robot_query: Query<&RobotState, With<Robot>>,
+    mut elements_query: Query<(&mut ElementCarte, &Position)>,
+) {
+    // On met à jour quand le robot est à la base
+    let robot_at_base = robot_query.iter().any(|state| matches!(state, RobotState::AtBase));
+
+    if robot_at_base {
+        for (mut element_carte, _) in elements_query.iter_mut() {
+            if element_carte.est_decouvert == EtatDecouverte::EnAttente {
+                element_carte.est_decouvert = EtatDecouverte::Decouvert;
+            }
+        }
+    }
+}
+
+fn assign_targets(
+    mut commands: Commands,
+    mut query: Query<(&mut Robot, &Position)>,
+    map_query: Query<&Carte>, // Query Carte as a component
+) {
+    if let Ok(carte) = map_query.get_single() {
+        for (mut robot, robot_pos) in query.iter_mut() {
+            if robot.type_robot == TypeRobot::Explorateur && robot.target_position.is_none() {
+                let target_x = rand::thread_rng().gen_range(0..carte.largeur) as i32;
+                let target_y = rand::thread_rng().gen_range(0..carte.hauteur) as i32;
+                robot.target_position = Some(Position { x: target_x, y: target_y });
+                robot.steps_moved = 0; 
+            }
+        }
+    }
+}
+
 fn main() {
+    let seed_option = request_seed_from_user();
+
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -404,11 +593,17 @@ fn main() {
             ..default()
           }))
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.5)))
+        .insert_resource(AffichageCasesNonDecouvertes(true))
+        .insert_resource(SeedResource { seed: seed_option })
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup_map)
         .add_systems(PostStartup, setup_bordures)
         .add_systems(PostStartup, spawn_robots)
         .add_systems(Update, move_robots_on_map_system)
+        .add_systems(Update, toggle_cases_non_decouvertes)
+        .add_systems(Update, adjust_visibility_system)
+        .add_systems(PostUpdate, discover_elements)
+        .add_systems(Update, update_robot_state)
         .add_systems(Update, collect_resources_system)
         .add_systems(Update, move_camera_system)
         .add_systems(Update, zoom_camera_system)
