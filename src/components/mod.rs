96d64c6cf5af4170b03d3605ea78f7dfe723dcab
Florent-W <35995337+Florent-W@users.noreply.github.com>
2024-05-04 18:59:56 +0200

    Ajustement du volume de la musique de fond et des bruitages

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,12 +3,9 @@ mod map;
 mod robot;
 mod resource;
 mod ui;
-mod audio;
 
 // Ré-exportation publique des composants
 pub use self::map::*;
 pub use self::robot::*;
 pub use self::resource::*;
 pub use self::ui::*;
-pub use self::audio::*;
-
---
+++ src/systems/audio.rs
@@ -0,0 +1,23 @@
+use bevy_kira_audio::{Audio, AudioChannel, AudioControl};
+use bevy_kira_audio::AudioSource;
+use bevy::prelude::*;
+
+// Fonction pour démarrer la musique de fond
+pub fn start_audio(
+    asset_server: Res<AssetServer>,
+    audio: ResMut<Audio>,
+) {
+    let music_handle: Handle<AudioSource> = asset_server.load("music.ogg");
+
+    audio.set_volume(0.4); 
+    audio.play(music_handle).looped(); 
+}
+
+pub fn encaissement_audio(
+    asset_server: &Res<AssetServer>,
+    audio: &Res<Audio>,
+) {
+    let effect_handle: Handle<AudioSource> = asset_server.load("encaissement.ogg"); 
+    audio.play(effect_handle).with_volume(0.1); 
+}
+
